d9622d022deee257dc140a37623071987011e27e
Christophe Eymard <christophe.eymard@sales-way.com>
2021-04-26 16:50:09 +0200

    preliminary sharp support

--- src/page.ts
+++ src/page.ts
@@ -733,6 +733,34 @@ export class Page {
     return pg
   }
 
+  sharp(filename: string, args: any) {
+    let look = this.lookup_file(filename)
+    const sh = require('sharp') as typeof import('sharp')
+    let aj = JSON.stringify(args)
+    let md5 = require('crypto').createHash('md5').update(aj, 'binary').digest('base64') as string
+
+    let res = `${look.local_dir}/${look.noext_basename}-${md5.slice(0, 8)}.${look.extension}`
+    let url = pth.join(this.$$params.assets_url, res)
+    let copy_path = pth.join(this.$$params.assets_out_dir, res)
+
+    let st = fs.existsSync(copy_path) ? fs.statSync(copy_path) : null
+    if (this.$$site.jobs.has(copy_path) || st?.mtimeMs! >= look.stats.mtimeMs) return url
+
+    this.$$site.jobs.set(copy_path, async () => {
+
+      let result_img = sh(look.absolute_path)
+      // console.dir(result_img.resize)
+      for (let a of args) {
+        let method = a[0]
+        let args = a.slice(1)
+        result_img = (result_img as any)[method].apply(result_img, args)
+      }
+      await result_img.toFile(copy_path)
+    })
+
+    return url
+  }
+
   get_this_page_in(genname: string) {
     let self: Page = this
     while (self.page) { self = self.page }
